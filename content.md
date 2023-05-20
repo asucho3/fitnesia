3ffe2a7d1f5844cdb35bef716cf52d188dc4c947
Alan Suchowolski <asucho3@gmail.com>
2023-05-20 11:22:57 -0300

    initial commit

---
+++ content.md
@@ -0,0 +1,83 @@
+# Hero
+
+Achieve your fitness goals
+
+Track your progress, stay motivated, and reach your ideal form with cutting-edge, AI and VR powered technology
+
+Begin your journey | Learn more
+
+# Features
+
+## Horizontally aligned icons
+
+1- Log your workouts, track your progress, and stay on top of your fitness goals
+2- Get personalized workout plans, progress tracking, and real-time feedback
+3- Rate your workout and share your progress with your friends
+
+# How it works
+
+## Z Pattern
+
+1- Tell us your preferred routines: we offer a tailored experience for each and every person. Our AI-powered technology will generate the ideal workout for you.
+2- Track your progress: our app offers built-in features for geo-tracking, progress logging and real-time achievement generation to help you stay motivated
+3- Lose it without going out: we also feature the most advanced virtual reality fitness classes on the market
+
+# Workouts
+
+## Horizontally aligned
+
+Our AI will help you choose from over 10.000 routines
+
+1- Low intensity:
+15-20 minutes
+300 calories
+
+2- Medium
+30-45 minutes
+450 calories
+
+3- High
+1 hour
+800 calories
+
+# Pricing
+
+- Free: 2 routines per day, performance tracking, share your progress, no VR gym
+- Full: unlimited routines per day, performance tracking, share your progress, VR gym
+
+# Testimonials
+
+## Carrousel
+
+"I've been using this app for just a few weeks, and I'm already seeing incredible results! The performance tracking feature has been a game-changer for me, and I love the personalized workout plans. This app has everything I need to stay motivated and on track towards my fitness goals." - Jane Doe
+"I never thought tracking my fitness could be so much fun! The virtual reality fitness classes are amazing - it's like I'm actually in the gym with my trainer." - John Smith
+"As a busy professional, I don't have a lot of time to spend at the gym. But with this app, I can get a great workout in no matter where I am." - Sarah Lee
+
+# CTA
+
+With features like personalized workout plans, progress tracking, and real-time feedback, you'll have everything you need to stay motivated and achieve the results you want
+
+Start now!
+
+# FAQ
+
+## Accordion
+
+- Is this service free?
+- Yes! We offer a completely free version of our software. Whenever you feel ready to take it to the next level, you can sign up for our full package.
+
+- How many times can I generate routines?
+- There is no limit to the total number of routines you can generate, regardless of the tier you have chosen. There is, however, a limit of 2 routines per day for the free tier.
+
+- Is the VR gym option available at all times?
+- Yes, the VR gym is always open. The classes are streamed live and can be accesed at any time afterwards.
+
+# Newsletter
+
+Be the first to know about new features, special discounts and more.
+Join our newsletter!
+
+
+# Footer
+
+Copyright, navigation, social apps
---
+++ script.js
@@ -0,0 +1,140 @@
+// TESTIMONIALS (slider component)
+
+const testimonialNav = document.querySelectorAll('.move');
+const testimonials = document.querySelectorAll('.testimonial');
+const toggleMenu = document.querySelector('.toggleMenu');
+
+const offset = 50;
+let currentIndex = 0;
+
+// moveTestimonials slides the testimonials left/right depending on which button the user clicks
+// whichever item has the dataset.code set to 0 will be the one to be showed (because (0-1)*50 = -50% and that's the translateX that centers the testimonial)
+
+const moveTestimonials = function (e) {
+  let direction;
+  if (e.target.classList.contains('next')) {
+    if (currentIndex === testimonials.length - 1) return;
+    direction = -1;
+  }
+  if (e.target.classList.contains('previous')) {
+    if (currentIndex === 0) return;
+    direction = 1;
+  }
+  currentIndex -= direction;
+
+  testimonials.forEach((el) => {
+    // hide all the elements
+    el.classList.add('hidden');
+    // apply transformation
+    el.dataset.code = String(+el.dataset.code + direction);
+    el.style.transform = `translate(${(el.dataset.code - 1) * offset}%, -50%)`;
+    // only show the item that has the dataset.code set to 0
+    if (el.dataset.code === '0') el.classList.remove('hidden');
+
+    // reveal both buttons
+    testimonialNav.forEach((el) => {
+      el.classList.remove('hidden');
+    });
+    if (currentIndex === 0) {
+      // hide the 'previous' button if this is the first item
+      testimonialNav.forEach((el) => {
+        if (el.classList.contains('previous')) {
+          el.classList.add('hidden');
+        }
+      });
+    }
+    if (currentIndex === testimonials.length - 1) {
+      // hide the 'previous' button if this is the last item
+      testimonialNav.forEach((el) => {
+        if (el.classList.contains('next')) {
+          el.classList.add('hidden');
+        }
+      });
+    }
+  });
+};
+
+// initialize the testimonials' positions
+const init = function () {
+  testimonials.forEach((el) => {
+    el.classList.add('hidden');
+    el.style.transform = `translate(${(el.dataset.code - 1) * offset}%, -50%)`;
+    if (el.dataset.code === '0') el.classList.remove('hidden');
+    // hide the 'previous' button
+    testimonialNav.forEach((el) => {
+      if (el.classList.contains('previous')) {
+        el.classList.add('hidden');
+      }
+    });
+  });
+};
+
+// add the event listeners for the testimonial navigation (previous/next)
+testimonialNav.forEach((el) =>
+  el.addEventListener('click', function (e) {
+    moveTestimonials(e);
+  })
+);
+
+init();
+
+// FAQ (accordion component)
+const faqContainer = document.querySelector('.faq-container');
+
+faqContainer.addEventListener('click', function (e) {
+  if (!e.target.closest('.faq-container')) return;
+  const allFaqItems = document.querySelectorAll('.faq-item');
+  allFaqItems.forEach((el) => {
+    if (el === e.target.closest('.faq-item')) return;
+    el.classList.remove('active');
+    el.querySelector('.faq-text').classList.add('collapse');
+  });
+  const faqItem = e.target.closest('.faq-item');
+  faqItem.classList.toggle('active');
+  const faqText = faqItem.querySelector('.faq-text');
+  faqText.classList.toggle('collapse');
+
+  const newsletter = document.querySelector('.newsletter-container');
+  if (faqItem.classList.contains('active')) {
+    newsletter.style.marginTop = '5.7rem';
+    return;
+  }
+  newsletter.style.marginTop = '8.6rem';
+});
+
+// Sticky nav
+const header = document.querySelector('.header');
+const hero = document.querySelector('.section-hero');
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    hero.style.marginTop = `${header.getBoundingClientRect().height}px`;
+    header.classList.add('sticky');
+  }
+  if (entry.isIntersecting) {
+    header.classList.remove('sticky');
+    hero.style.marginTop = '0';
+  }
+};
+
+const navObserver = new IntersectionObserver(stickyNav, {
+  threshold: 0.1,
+});
+
+navObserver.observe(hero);
+
+// Navigation
+document.body.addEventListener('click', function (e) {
+  if (!e.target.classList.contains('nav-link')) return;
+  document.querySelector(`${e.target.getAttribute('href')}`).scrollIntoView({
+    alignToTop: true,
+    behavior: 'smooth',
+  });
+});
+
+// Mobile menu
+toggleMenu.addEventListener('click', function (e) {
+  if (!e.target.classList.contains('toggleMenu')) return;
+  header.classList.toggle('visible');
+});
